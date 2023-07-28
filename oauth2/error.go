fdee3d66da6a586cfe28d3f0b7eb5bdc495e0a50
skay <skai-zhang@hotmail.com>
2023-07-28 15:26:27 +0800

    update error code

--- oauth2/error.go
+++ oauth2/error.go
@@ -19,19 +19,44 @@ func (e *CustomError) Throw() error {
 }
 
 type CustomErrors struct {
-	ImplementNotFound  CustomError
-	InvalidRequest     CustomError
+	// 未找到实现
+	ImplementNotFound CustomError
+	// 无效请求
+	InvalidRequest CustomError
+	// 未经授权的客户端
 	UnauthorizedClient CustomError
-	InvalidAuthHeader  CustomError
+	// 授权请求头无效
+	InvalidAuthHeader CustomError
+	// 授权信息无效
 	InvalidAuthMessage CustomError
-	Unexpected         CustomError
+	// 授权请求类型无效
+	InvalidAuthRequestType CustomError
+	// 重定向地址无效
+	InvalidRedirectUri CustomError
+	// 地址方案无效
+	InvalidUriScheme CustomError
+	// 地址主机无效
+	InvalidUriHosts CustomError
+
+	// 拒绝访问
+	DeniedAccess CustomError
+	// URI中禁止出现次级资源
+	ProhibitFragment CustomError
+	// 意料之外的错误
+	Unexpected CustomError
 }
 
 var Errors = CustomErrors{
-	ImplementNotFound:  CustomError{10000, "未找到实现"},
-	InvalidRequest:     CustomError{10001, "无效请求"},
-	UnauthorizedClient: CustomError{10002, "未经授权的请求"},
-	InvalidAuthHeader:  CustomError{10010, "授权请求头无效"},
-	InvalidAuthMessage: CustomError{10011, "授权信息无效"},
-	Unexpected:         CustomError{99999, "意料之外的错误"},
+	ImplementNotFound:      CustomError{10000, "未找到实现"},
+	InvalidRequest:         CustomError{10001, "无效请求"},
+	UnauthorizedClient:     CustomError{10002, "未经授权的客户端"},
+	InvalidAuthHeader:      CustomError{10010, "授权请求头无效"},
+	InvalidAuthMessage:     CustomError{10011, "授权信息无效"},
+	InvalidAuthRequestType: CustomError{10012, "授权请求类型无效"},
+	InvalidRedirectUri:     CustomError{10013, "重定向地址无效"},
+	InvalidUriScheme:       CustomError{10013, "地址方案无效"},
+	InvalidUriHosts:        CustomError{10013, "地址主机无效"},
+	DeniedAccess:           CustomError{10014, "拒绝访问"},
+	ProhibitFragment:       CustomError{10015, "URI中禁止出现次级资源"},
+	Unexpected:             CustomError{99999, "意料之外的错误"},
 }
