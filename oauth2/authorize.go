d34a0e415ce1bddd52c3c23637a982c1a7c5cca4
skay <skai-zhang@hotmail.com>
2023-07-28 15:21:58 +0800

    add util and authorize

---
+++ oauth2/authorize.go
@@ -0,0 +1,254 @@
+package oauth2
+
+import (
+	"net/http"
+	"net/url"
+	"regexp"
+	"time"
+)
+
+var (
+	pkceMatcher = regexp.MustCompile("^[a-zA-Z0-9~._-]{43,128}$")
+)
+
+type AuthorizeRequestType string
+
+type AuthorizeRequest struct {
+	// 请求授权类型
+	Type AuthorizeRequestType
+	// 客户端
+	Client Client
+	// 过期时间
+	ExpiresIn int32
+	// 请求范围
+	Scope string
+	// 重定向地址
+	RedirectUri string
+	// 状态
+	State string
+	// 用户信息
+	UserData interface{}
+	// 挑战代码
+	CodeChallenge string
+	// 挑战方法
+	CodeChallengeMethod string
+
+	// 请求是否被授权
+	Authorized bool
+	// 请求对象
+	HttpRequest *http.Request
+}
+
+type Authorize struct {
+	// 客户端
+	Client Client
+	// 授权码
+	Code string
+	// 过期时间
+	ExpiresIn int32
+	// 请求范围
+	Scope string
+	// 重定向地址
+	RedirectUri string
+	// 状态
+	State string
+	// 创建时间
+	CreatedAt time.Time
+	// 用户信息
+	UserData interface{}
+	// 挑战代码
+	CodeChallenge string
+	// 挑战方法
+	CodeChallengeMethod string
+}
+
+// 授权是否已过期
+func (d *Authorize) IsExpired() bool {
+	return d.IsExpiredAt(time.Now())
+}
+
+// 指定时间时是否已过期
+func (d *Authorize) IsExpiredAt(t time.Time) bool {
+	return d.ExpireAt().Before(t)
+}
+
+// 获取过期时间
+func (d *Authorize) ExpireAt() time.Time {
+	return d.CreatedAt.Add(time.Duration(d.ExpiresIn) * time.Second)
+}
+
+// 创建授权令牌
+type AuthorizeTokenGen interface {
+	GenerateAuthorizeToken(data *Authorize) (string, error)
+}
+
+// 授权请求处理
+func (s *Server) HandleAuthorizeRequest(res *Response, req *http.Request) *AuthorizeRequest {
+	req.ParseForm()
+
+	// 获取重定向地址
+	redirectUri, err := url.QueryUnescape(req.FormValue("redirect_uri"))
+	if err != nil {
+		res.SetErrorState(Errors.InvalidRequest.Message, "", "")
+		res.InternalError = err
+		return nil
+	}
+
+	// 创建授权请求
+	ret := &AuthorizeRequest{
+		State:       req.FormValue("state"),
+		Scope:       req.FormValue("scope"),
+		RedirectUri: redirectUri,
+		Authorized:  false,
+		HttpRequest: req,
+	}
+
+	// 获取客户端信息
+	ret.Client, err = res.Store.GetClient(req.FormValue("client_id"))
+	// 客户端接口未实现
+	if err == Errors.ImplementNotFound.Throw() {
+		res.SetErrorState(Errors.ImplementNotFound.Message, "", ret.State)
+		return nil
+	}
+	// 客户端获取出错
+	if err != nil {
+		res.SetErrorState(Errors.Unexpected.Message, "", ret.State)
+		res.InternalError = err
+		return nil
+	}
+	// 未获取到客户端
+	if ret.Client == nil {
+		res.SetErrorState(Errors.UnauthorizedClient.Message, "", ret.State)
+		return nil
+	}
+	// 未获取到重定向地址(必须要有)
+	if ret.Client.GetRedirectUri() == "" {
+		res.SetErrorState(Errors.UnauthorizedClient.Message, "", ret.State)
+		return nil
+	}
+	// 允许多个重定向地址
+	if ret.RedirectUri != "" && s.Config.AllowMultipleRedirectUri {
+		ret.RedirectUri = GetFirstUri(ret.Client.GetRedirectUri())
+	}
+	// 校验重定向地址是否匹配
+	if realRedirectUri, err := ValidateUriList(ret.Client.GetRedirectUri(), ret.RedirectUri, s.Config.AllowMultipleRedirectUri); err != nil {
+		res.SetErrorState(Errors.InvalidRequest.Message, "", ret.State)
+		res.InternalError = err
+		return nil
+	} else {
+		ret.RedirectUri = realRedirectUri
+	}
+	// 设置重定向地址
+	res.SetRedirect(ret.RedirectUri)
+	// 获取请求类型
+	requestType := AuthorizeRequestType(req.FormValue("response_type"))
+	// 检查请求类型是否在允许范围
+	if s.Config.AllowedAuthorizeTypes.Allow(requestType) {
+		switch requestType {
+		case CODE:
+			ret.Type = CODE
+			ret.ExpiresIn = s.Config.AuthorizationExpiration
+			// 判断挑战代码是否为空
+			if codeChallenge := req.FormValue("code_challenge"); len(codeChallenge) == 0 {
+				// 判断是否要求请求使用PKCE挑战
+				// https://datatracker.ietf.org/doc/html/rfc7636
+				if s.Config.RequirePKCEForPublicClients && CheckClientSecret(ret.Client, "") {
+					res.SetErrorState(Errors.InvalidRequest.Message, "code_challenge (rfc7636) required for public clients", ret.State)
+					return nil
+				}
+			} else {
+				// 获取挑战方法
+				codeChallengeMethod := req.FormValue("code_challenge_method")
+				// 未传入则默认 plain
+				if len(codeChallengeMethod) == 0 {
+					codeChallengeMethod = PKCE_PLAIN
+				}
+				// 判断挑战方法是否在允许范围
+				if codeChallengeMethod != PKCE_PLAIN && codeChallengeMethod != PKCE_S256 {
+					res.SetErrorState(Errors.InvalidRequest.Message, "code_challenge_method transform algorithm not supported (rfc7636)", ret.State)
+					return nil
+				}
+				// 判断挑战代码是否包含非法字符
+				if matched := pkceMatcher.MatchString(codeChallenge); !matched {
+					res.SetErrorState(Errors.InvalidRequest.Message, "code_challenge invalid (rfc7636)", ret.State)
+					return nil
+				}
+				ret.CodeChallenge = codeChallenge
+				ret.CodeChallengeMethod = codeChallengeMethod
+			}
+		case TOKEN:
+			ret.Type = TOKEN
+			ret.ExpiresIn = s.Config.AccessExpiration
+		}
+		return ret
+	}
+	res.SetErrorState(Errors.InvalidAuthRequestType.Message, "", ret.State)
+	return nil
+}
+
+// 授权请求完成
+func (s *Server) FinishAuthorizeRequest(res *Response, req *http.Request, ar *AuthorizeRequest) {
+	// 处理出错时直接返回
+	if res.IsError {
+		return
+	}
+	// 设置重定向地址
+	res.SetRedirect(ar.RedirectUri)
+	// 判断是否已授权
+	if ar.Authorized {
+		// 授权请求类型是否为令牌
+		if ar.Type == TOKEN {
+			res.SetRedirectFragment(true)
+			// 请求创建访问令牌,此时不应生成刷新令牌
+			ret := &AccessRequest{
+				Type:            IMPLICIT,
+				Code:            "",
+				Client:          ar.Client,
+				RedirectUri:     ar.RedirectUri,
+				Scope:           ar.Scope,
+				GenerateRefresh: false,
+				Authorized:      true,
+				ExpiresIn:       ar.ExpiresIn,
+				UserData:        ar.UserData,
+			}
+			// 完成访问令牌请求
+			s.FinishAccessRequest(res, req, ret)
+			// 判断状态
+			if ar.State != "" && res.InternalError == nil {
+				res.Output["state"] = ar.State
+			}
+		} else {
+			// 创建授权令牌对象
+			ret := &Authorize{
+				Client:              ar.Client,
+				CreatedAt:           s.Now(),
+				ExpiresIn:           ar.ExpiresIn,
+				RedirectUri:         ar.RedirectUri,
+				State:               ar.State,
+				Scope:               ar.Scope,
+				UserData:            ar.UserData,
+				CodeChallenge:       ar.CodeChallenge,
+				CodeChallengeMethod: ar.CodeChallengeMethod,
+			}
+			// 授权令牌,获取授权码
+			code, err := s.AuthorizeTokenGen.GenerateAuthorizeToken(ret)
+			if err != nil {
+				res.SetErrorState(Errors.Unexpected.Message, "", ar.State)
+				res.InternalError = err
+				return
+			}
+			ret.Code = code
+			// 保存授权码
+			if err = res.Store.SaveAuthorize(ret); err != nil {
+				res.SetErrorState(Errors.Unexpected.Message, "", ar.State)
+				res.InternalError = err
+				return
+			}
+			// 返回授权码和穿透状态
+			res.Output["code"] = ret.Code
+			res.Output["state"] = ret.State
+		}
+	} else {
+		res.SetErrorState(Errors.DeniedAccess.Message, "", ar.State)
+	}
+}
---
+++ oauth2/util.go
@@ -0,0 +1,121 @@
+package oauth2
+
+import (
+	"crypto/subtle"
+	"errors"
+	"net"
+	"net/url"
+	"strings"
+)
+
+// 检查客户端密钥
+func CheckClientSecret(client Client, secret string) bool {
+	switch client := client.(type) {
+	// 实现了客户端密钥比对接口
+	case ClientSecretMatcher:
+		return client.ClientSecretMatches(secret)
+	default:
+		// 没有实现则明文校验
+		return subtle.ConstantTimeCompare([]byte(client.GetSecret()), []byte(secret)) == 1
+	}
+}
+
+// 获取第一个URI
+func GetFirstUri(baseUriList string) string {
+	slist := strings.Split(baseUriList, ",")
+	if len(slist) > 0 {
+		return slist[0]
+	}
+	return ""
+}
+
+// 校验URI列表
+func ValidateUriList(baseUriList string, redirectUri string, allow bool) (realRedirectUri string, err error) {
+	// 创建列表切片
+	var slist []string
+	if allow {
+		slist = strings.Split(baseUriList, ",")
+	} else {
+		slist = make([]string, 0)
+		slist = append(slist, baseUriList)
+	}
+	// 遍历列表
+	for _, sitem := range slist {
+		realRedirectUri, err = ValidateUri(sitem, redirectUri)
+		// 通过验证且为报错
+		if err == nil {
+			return realRedirectUri, nil
+		}
+	}
+
+	return "", Errors.InvalidRedirectUri.Throw()
+}
+
+// 验证URI
+func ValidateUri(baseUri string, redirectUri string) (realRedirectUri string, err error) {
+	// 传入为空
+	if baseUri == "" || redirectUri == "" {
+		return "", errors.New("urls cannot be blank.")
+	}
+	// 分析URL
+	base, redirect, err := AnalysisUrls(baseUri, redirectUri)
+	if err != nil {
+		return "", err
+	}
+	// 精准路径匹配
+	if base.Path == redirect.Path {
+		return redirect.String(), nil
+	}
+	// 前缀地址匹配
+	requiredPrefix := strings.TrimRight(base.Path, "/") + "/"
+	if !strings.HasPrefix(redirect.Path, requiredPrefix) {
+		return "", Errors.InvalidRedirectUri.Throw()
+	}
+	return redirect.String(), nil
+}
+
+// 分析URL
+func AnalysisUrls(baseUrl, redirectUrl string) (retBaseUrl, retRedirectUrl *url.URL, err error) {
+	var base, redirect *url.URL
+	// 分析URL
+	if base, err = url.Parse(baseUrl); err != nil {
+		return nil, nil, err
+	}
+	if redirect, err = url.Parse(redirectUrl); err != nil {
+		return nil, nil, err
+	}
+	// 出现次级资源(http://xxxx/xx#次级资源)
+	if base.Fragment != "" || redirect.Fragment != "" {
+		return nil, nil, Errors.ProhibitFragment.Throw()
+	}
+	// 方案不匹配
+	if redirect.Scheme != base.Scheme {
+		return nil, nil, Errors.InvalidUriScheme.Throw()
+	}
+
+	var (
+		redirectMatch bool
+		host          string
+	)
+
+	// 验证主机地址
+	if redirect.Host == base.Host {
+		redirectMatch = true
+		host = base.Host
+	} else if baseIP := net.ParseIP(base.Host); baseIP != nil && baseIP.IsLoopback() && base.Scheme == "http" {
+		// 回环地址砍掉端口
+		if redirectIP := net.ParseIP(redirect.Hostname()); redirectIP != nil && redirectIP.IsLoopback() {
+			redirectMatch = true
+			host = redirect.Host
+		}
+	}
+	// 主机不匹配
+	if !redirectMatch {
+		return nil, nil, Errors.InvalidUriHosts.Throw()
+	}
+
+	// 返回解析数据
+	retBaseUrl = (&url.URL{Scheme: base.Scheme, Host: host}).ResolveReference(&url.URL{Path: base.Path})
+	retRedirectUrl = (&url.URL{Scheme: base.Scheme, Host: host}).ResolveReference(&url.URL{Path: redirect.Path, RawQuery: redirect.RawQuery})
+	return retBaseUrl, retRedirectUrl, nil
+}
