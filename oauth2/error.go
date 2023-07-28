64fadf7030d93cebcb061f96d7bc2ddcd1294473
skay <skai-zhang@hotmail.com>
2023-07-28 12:26:44 +0800

    Update error.go

--- oauth2/error.go
+++ oauth2/error.go
@@ -19,15 +19,19 @@ func (e *CustomError) Throw() error {
 }
 
 type CustomErrors struct {
+	ImplementNotFound  CustomError
 	InvalidRequest     CustomError
 	UnauthorizedClient CustomError
 	InvalidAuthHeader  CustomError
 	InvalidAuthMessage CustomError
+	Unexpected         CustomError
 }
 
 var Errors = CustomErrors{
-	InvalidRequest:     CustomError{10000, "无效请求"},
-	UnauthorizedClient: CustomError{10001, "未经授权的请求"},
+	ImplementNotFound:  CustomError{10000, "未找到实现"},
+	InvalidRequest:     CustomError{10001, "无效请求"},
+	UnauthorizedClient: CustomError{10002, "未经授权的请求"},
 	InvalidAuthHeader:  CustomError{10010, "授权请求头无效"},
 	InvalidAuthMessage: CustomError{10011, "授权信息无效"},
+	Unexpected:         CustomError{99999, "意料之外的错误"},
 }
